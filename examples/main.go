6ab0f97563314576c63cda24a4204882f4cca654
dzlab <dzlabs@outlook.com>
2016-04-02 17:10:48 +0200

    added example of search query

--- examples/main.go
+++ examples/main.go
@@ -18,6 +18,7 @@ func main() {
 	client.Bulk("my_store", "products").AddOperation(e.NewOperation(1).Add("price", 10).Add("productID", "XHDK-A-1293-#fJ3")).AddOperation(e.NewOperation(2).Add("price", 20).Add("productID", "KDKE-B-9947-#kL5")).AddOperation(e.NewOperation(3).Add("price", 30).Add("productID", "JODL-X-1937-#pV7")).AddOperation(e.NewOperation(4).Add("price", 30).Add("productID", "QQPX-R-3956-#aD8")).Post()
 	client.Search("my_store", "products").AddQuery(e.NewQuery("query").AddQuery(e.NewQuery("filtered").AddQuery(e.NewQuery("query").AddQuery(e.NewQuery("match_all"))).AddQuery(e.NewQuery("filter").AddQuery(e.NewQuery("term").Add("price", 30))))).Get()
 	client.Analyze("my_store").Field("productID").Get("XHDK-A-1293-#fJ3")
+	client.Search("my_store", "products").AddQuery(e.NewQuery("query").AddQuery(e.NewQuery("filtered").AddQuery(e.NewQuery("filter").AddQuery(e.NewBool().AddShould(e.NewQuery("term").Add("price", 20)).AddShould(e.NewQuery("term").Add("productID", "XHDK-A-1293-#fJ3")).AddMustNot(e.NewTerm().Add("price", 30)))))).Get()
 	//"{mappings: {tweet: {properties: {tweet:{type: \"string\", analyzer: \"english\"}, date: {type: \"date\"}, name: {type: \"string\"}, user_id: {type: \"long\"}}}}}"
 	client.Index("gb_v1").Put()
 	client.Index("gb_v1").SetAlias("gb").Put()
