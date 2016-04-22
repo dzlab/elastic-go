0b34d39b5bc7024145ec75550b67b566dadcf0de
dzlab <dzlabs@outlook.com>
2016-04-22 10:59:21 +0200

    fix for golint

--- analyze.go
+++ analyze.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// Analyze a structure representing an Elasticsearch query for the Analyze API
 type Analyze struct {
 	client   *Elasticsearch
 	parser   Parser
@@ -13,11 +14,13 @@ type Analyze struct {
 }
 
 const (
+	// ANALYZE a constant for Analyze query name
 	ANALYZE = "analyze"
 )
 
+// Analyze returns an new Analyze request on the given index
 func (client *Elasticsearch) Analyze(index string) *Analyze {
-	var url string = client.request(index, "", -1, ANALYZE)
+	url := client.request(index, "", -1, ANALYZE)
 	return &Analyze{
 		client: client,
 		parser: &AnalyzeResultParser{},
@@ -25,25 +28,20 @@ func (client *Elasticsearch) Analyze(index string) *Analyze {
 	}
 }
 
-/*
- * Analyze a field
- */
+// Field adds a field to an anlyze request
 func (analyzer *Analyze) Field(field string) *Analyze {
 	analyzer.field = field
 	return analyzer
 }
 
-/*
- * Analyze an analyzer given by name
- */
+// Analyzer adds a named standard Elasticsearch analyzer to the Analyze query
 func (analyzer *Analyze) Analyzer(name string) *Analyze {
 	analyzer.analyzer = name
 	return analyzer
 }
 
-/*
- * GET /:index/_analyze?field=field_name
- */
+// Get submits an Analyze query to Elasticsearch
+// GET /:index/_analyze?field=field_name
 func (analyzer *Analyze) Get(body string) {
 	// construct the url
 	url := analyzer.url
