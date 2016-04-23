95f40d344825f0f4dcbd8cac5976eab9b5cef973
dzlab <dzlabs@outlook.com>
2016-04-23 15:03:06 +0200

    fix for golint

--- insert.go
+++ insert.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 )
 
-/*
- * a request representing an insert
- */
+// Insert a request representing a document insert query
 type Insert struct {
 	client *Elasticsearch
 	parser Parser
@@ -15,11 +13,9 @@ type Insert struct {
 	doc    interface{}
 }
 
-/*
- * Create an Insert request, that will submit a new document to elastic search
- */
+// Insert Create an Insert request, that will submit a new document to elastic search
 func (client *Elasticsearch) Insert(index, doctype string) *Insert {
-	var url string = fmt.Sprintf("http://%s/%s/%s", client.Addr, index, doctype)
+	url := fmt.Sprintf("http://%s/%s/%s", client.Addr, index, doctype)
 	return &Insert{
 		client: client,
 		parser: &InsertResultParser{},
@@ -27,33 +23,25 @@ func (client *Elasticsearch) Insert(index, doctype string) *Insert {
 	}
 }
 
-/*
- * Create a new Insert query (for test purpose)
- */
+// newInsert Create a new Insert query (for test purpose)
 func newInsert() *Insert {
 	return &Insert{}
 }
 
-/*
- * Set the document to insert
- */
+// Document set the document to insert
 func (insert *Insert) Document(id int64, doc interface{}) *Insert {
 	insert.id = id
 	insert.doc = doc
 	return insert
 }
 
-/*
- * Get a string representation of the document
- */
+// String returns a string representation of the document
 func (insert *Insert) String() string {
 	return String(insert.doc)
 }
 
-/*
- * request mappings between the json fields and how Elasticsearch store them
- * PUT /:index/:type/:id
- */
+// Put submits a request mappings between the json fields and how Elasticsearch store them
+// PUT /:index/:type/:id
 func (insert *Insert) Put() {
 	// construct the url
 	url := fmt.Sprintf("%s/%d", insert.url, insert.id)
