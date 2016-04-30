cf03cad523742900be175fec9db3203cc9edca4c
dzlab <dzlabs@outlook.com>
2016-04-30 18:16:53 +0200

    using params dictionnary instead of separate fields

--- analyze.go
+++ analyze.go
@@ -1,16 +1,13 @@
 package elastic
 
-import (
-	"fmt"
-)
+import ()
 
 // Analyze a structure representing an Elasticsearch query for the Analyze API
 type Analyze struct {
-	client   *Elasticsearch
-	parser   Parser
-	url      string
-	field    string
-	analyzer string
+	client *Elasticsearch
+	parser Parser
+	url    string
+	params map[string]string
 }
 
 const (
@@ -25,18 +22,25 @@ func (client *Elasticsearch) Analyze(index string) *Analyze {
 		client: client,
 		parser: &AnalyzeResultParser{},
 		url:    url,
+		params: make(map[string]string),
 	}
+}
+
+// Pretty pretiffies the response result
+func (analyzer *Analyze) Pretty() *Analyze {
+	analyzer.params["pretty"] = ""
+	return analyzer
 }
 
 // Field adds a field to an anlyze request
 func (analyzer *Analyze) Field(field string) *Analyze {
-	analyzer.field = field
+	analyzer.params["field"] = field
 	return analyzer
 }
 
 // Analyzer adds a named standard Elasticsearch analyzer to the Analyze query
 func (analyzer *Analyze) Analyzer(name string) *Analyze {
-	analyzer.analyzer = name
+	analyzer.params["analyzer"] = name
 	return analyzer
 }
 
@@ -44,12 +48,8 @@ func (analyzer *Analyze) Analyzer(name string) *Analyze {
 // GET /:index/_analyze?field=field_name
 func (analyzer *Analyze) Get(body string) {
 	// construct the url
-	url := analyzer.url
-	if analyzer.field != "" {
-		url = fmt.Sprintf("%s?field=%s", url, analyzer.field)
-	} else if analyzer.analyzer != "" {
-		url = fmt.Sprintf("%s?analyzer=%s", url, analyzer.analyzer)
-	}
+	url := urlString(analyzer.url, analyzer.params)
+
 	// construct the body
 	analyzer.client.Execute("GET", url, body, analyzer.parser)
 }
