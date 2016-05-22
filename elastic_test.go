36fb7e33f6a39b2502cd602adaeafa40ad1045cd
dzlab <dzlabs@outlook.com>
2016-05-22 11:08:54 +0200

    fixed recurrent failing test

--- elastic_test.go
+++ elastic_test.go
@@ -12,13 +12,16 @@ func TestUrlString(t *testing.T) {
 	actual := []string{
 		urlString("/", Map{}),
 		urlString("?", Map{"k1": "v1"}),
-		urlString("/", Map{"k1": "", "k2": "v2"}),
 	}
 	// expected result
 	expected := []string{
 		"/",
 		"?k1=v1",
-		"/?k1&k2=v2",
 	}
 	equals(t, actual, expected)
+	// tests for cases where order doesn't matter
+	str := urlString("/", Map{"k1": "", "k2": "v2"})
+	if str != "/?k1&k2=v2" && str != "/?k2=v2&k1" {
+		t.Errorf("%s should be equal to %s or %s", str, "/?k1&k2=v2", "/?k2=v2&k1")
+	}
 }
